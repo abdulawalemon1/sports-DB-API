4aebb9206d2a821aeb00f4beef3cd7555e7fd658
abdulawalemon1 <emondx4@gmail.com>
2022-02-28 02:28:41 +0600

    Details section and error handling added

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
 const parent = document.getElementById('player-container');
 const searchPlayer = () => {
+    parent.innerHTML = '';
+    document.getElementById('spinner').style.display = 'block'
     const searchField = document.getElementById('search-box');
     const searchValue = searchField.value;
     const url = `https://www.thesportsdb.com/api/v1/json/2/searchplayers.php?p=${searchValue}`
     fetch(url)
         .then(res => res.json())
-        .then(data => showPlayerDetails(data.player))
-    searchField.innerText = '';
-    // parent.innerHTML=
-};
+        .then(data => {
+            if (data.player == null) {
+                document.getElementById('spinner').style.display = 'block';
+            } else {
+
+                showPlayerDetails(data.player);
+                document.getElementById('spinner').style.display = "none";
+            }
+
+        });
+}
 
 const showPlayerDetails = (players) => {
-    console.log(players)
+
+
     for (const player of players) {
 
         const div = document.createElement('div');
@@ -46,5 +56,27 @@ const getDetails = (idPlayer) => {
         .then(data => setDetails(data.players[0]));
 }
 const setDetails = (info) => {
-    console.log(info)
+    if (info.strGender == "Male") {
+        document.getElementById('male').style.display = 'block';
+        document.getElementById('female').style.display = 'none';
+    } else {
+        document.getElementById('female').style.display = 'block';
+        document.getElementById('male').style.display = 'none';
+
+    }
+    document.getElementById('detail-container').innerHTML = `
+    <div>
+        <h2>Player Details</h2>
+        <img class="img-fluid" src="${info.strThumb}" alt="">
+        <h1>Name: ${info.strPlayer}</h1>
+        <h5>Gender: ${info.strGender}</h5>
+        <h5>Height: ${info.strHeight}</h5>
+        <h5>Country: ${info.strNationality}</h5>
+        <h5>Sport: ${info.strSport}</h5>
+        <h5>Teams: ${info.strTeam},${info.strTeam2}</h5>
+        <h5>Position: ${info.strPosition}</h5>
+        <h5>Jercy Number: ${info.strNumber}</h5>
+        <h5>Wage:${info.strWage}</h5>
+        
+    </div>`;
 }
